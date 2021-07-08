591abdf71c12bba238d7bd2434c9ed73326ab526
Binesh <piratebinesh@gmail.com>
2021-07-08 19:37:16 +0530

    Completed Linked List DS.

--- lib/data_structures/linked_list.dart
+++ lib/data_structures/linked_list.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
-class LinkedListDS extends StatelessWidget {
+class LinkedListDS extends StatefulWidget {
+  @override
+  _LinkedListDSState createState() => _LinkedListDSState();
+}
+
+class _LinkedListDSState extends State<LinkedListDS> {
+  static LinkedListOperations _listOperations;
+
+  @override
+  void initState() {
+    _listOperations = new LinkedListOperations();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -35,7 +48,7 @@ class LinkedListDS extends StatelessWidget {
           body: TabBarView(
             children: [
               LinkedListImplementation(),
-              LinkedlistInfo()
+              LinkedListInfo()
             ],
           ),
         )
@@ -49,7 +62,8 @@ class LinkedListImplementation extends StatefulWidget {
 }
 
 class _LinkedListImplementationState extends State<LinkedListImplementation> {
-  LinkedListOperations _listOperations = new LinkedListOperations();
+  // LinkedListOperations _listOperations = new LinkedListOperations();
+  LinkedListOperations _listOperations = _LinkedListDSState._listOperations;
 
   TextEditingController deleteDataController = new TextEditingController();
 
@@ -515,7 +529,7 @@ class _LinkedListImplementationState extends State<LinkedListImplementation> {
                                   fontSize: 20.0
                               ),
                             ):
-                            Text('Empty List'),
+                            Text(''),
                     ),
                   ),
                 ),
@@ -675,11 +689,118 @@ class LinkedListOperations{
 }
 
 
-class LinkedlistInfo extends StatelessWidget {
+class LinkedListInfo extends StatelessWidget {
+
+  final List<String> list = [
+    'Yellow color denotes the Head nodes data',
+    'Orange color denotes the pointer of the Head node',
+    'Blue color denotes data',
+    'Green color denotes pointer to the next data',
+    '"Insert" inserts data into the Linked List',
+    '"Delete" deletes the element from the Linked List',
+  ];
+
+  List<Widget> infoWidget(){
+    List<Widget> l = [];
+
+    for(String info in list){
+      l.add(
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                Flexible(
+                  child: Text(
+                    info,
+                    style: TextStyle(
+                        fontSize: 20.0
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+      );
+
+      l.add(
+        SizedBox(
+          height: 20,
+        ),
+      );
+    }
+
+    return l;
+  }
+
+  Widget displayNode(){
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: 200,
+          height: 50,
+          child: Row(
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        border: Border.all(color: Colors.black, width: 1.0, style: BorderStyle.solid)
+                    ),
+                    child: Center(
+                        child: Text(
+                          'Data',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                    ),
+                  )
+              ),
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          border: Border.all(color: Colors.black, width: 1.0, style: BorderStyle.solid)
+                      ),
+                      child: Center(
+                          child: Text(
+                              'Pointer',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold
+                            ),
+                          )
+                      )
+                  )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            displayNode(),
+            SizedBox(
+              height: 20.0,
+            ),
+            Column(
+              children: infoWidget(),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
